4180df756af5ae6a51057cbe85fb9444dcbc553a
刘佳 <991609424@qq.com>
2016-11-15 18:35:44 +0800

    创建首页，banner功能

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 //app.js
 App({
   onLaunch: function () {
-    //调用API从本地缓存中获取数据
+    // 调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
+    // 获取当前时间戳
+    var timestamp = Data.now()
+    // unshift() 方法可向数组的开头添加一个或更多元素，并返回新的长度
+    logs.unshift(timestamp)
+    // 调用API，将数据进行本地缓存
     wx.setStorageSync('logs', logs)
   },
   getUserInfo:function(cb){
